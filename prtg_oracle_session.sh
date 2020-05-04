f809b6f7fbb5f9d830214953ee49e038858d58d2
Airlian Her <airlian@gmail.com>
2020-05-04 16:08:27 +0800

    Create prtg_oracle_session.sh

---
+++ prtg_oracle_session.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+if [ "$1" == '' ]; then
+   exit
+fi
+prtg=$(/u01/app/oracle/product/11.2.0/xe/bin/sqlplus -s /nolog <<BEGIN_S
+CONNECT omni/omni
+SET FEEDBACK off 
+SET PAGESIZE 0 
+select '<prtg>' from dual;
+set serveroutput on;
+declare
+    l_group varchar2(100);
+    l_count number;
+    cursor session_cur is
+    select $1, count(*) ccc into l_group, l_count from v\$session group by $1;
+begin
+    open session_cur;
+    loop
+    fetch session_cur into l_group, l_count;
+    exit when session_cur%NOTFOUND;
+        dbms_output.put_line('<result>');
+        dbms_output.put_line('<channel>'|| l_group ||'</channel>');
+        dbms_output.put_line('<value>'|| l_count ||'</value>');
+        dbms_output.put_line('</result>');
+    end loop;
+end;
+/
+select '</prtg>' from dual;
+BEGIN_S
+)
+
+echo $prtg
+url="http://192.168.20.49:5050/$1"
+echo $url
+/usr/bin/curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -s -k -m 10 "$url" -d "content=$prtg"
+echo
